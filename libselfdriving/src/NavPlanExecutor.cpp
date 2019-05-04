cabdb1a2a5d06008bba2e5894aa232075613d963
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-05-04 18:33:53 +0200

    initial tree structure

---
+++ libselfdriving/src/NavPlanExecutor.cpp
@@ -0,0 +1,7 @@
+/* -------------------------------------------------------------------------
+ *   SelfDriving C++ library based on PTGs and mrpt-nav
+ * Copyright (C) 2019 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+
+#include <libselfdriving/NavPlanExecutor.h>
