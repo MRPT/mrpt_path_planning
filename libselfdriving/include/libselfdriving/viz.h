48b998536779868319d87455f8929503e7ce0805
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-07-18 14:09:44 +0200

    viz ptg segments

--- libselfdriving/include/libselfdriving/viz.h
+++ libselfdriving/include/libselfdriving/viz.h
@@ -15,6 +15,9 @@ struct NavPlanRenderOptions
     /** dont return viz_nav_plan() until user closes the window */
     bool gui_modal{true};
     int  show_robot_shape_every_N{1};
+
+    /** Render the PTG path segments between key poses */
+    bool show_ptg_path_segments{true};
 };
 
 void viz_nav_plan(
--- libselfdriving/src/bestTrajectory.cpp
+++ libselfdriving/src/bestTrajectory.cpp
@@ -5,6 +5,9 @@
  * ------------------------------------------------------------------------- */
 
 #include <libselfdriving/bestTrajectory.h>
+#include <mrpt/core/get_env.h>
+
+static const bool VERBOSE = mrpt::get_env<bool>("VERBOSE", false);
 
 using namespace selfdrive;
 
@@ -25,19 +28,14 @@ bool selfdrive::bestTrajectory(
     mrpt::math::TPose2D  best_ptg_relPose;
     mrpt::math::TTwist2D best_ptg_velAtEnd;
 
+    // Make PTG realize of current kinematic state:
+    const auto newDyn = npa.getPTGDynState();
+
     // For each ptg:
     for (unsigned int ptg_idx = 0; ptg_idx < trs.ptgs.size(); ptg_idx++)
     {
         auto& ptg = trs.ptgs[ptg_idx];
 
-        // Make PTG realize of current kinematic state:
-        mrpt::nav::CParameterizedTrajectoryGenerator::TNavDynamicState newDyn;
-        newDyn.relTarget   = relPose;
-        newDyn.curVelLocal = npa.state_from.vel;
-        // Global to local velocity:
-        newDyn.curVelLocal.rotate(-npa.state_from.pose.phi);
-        MRPT_TODO("Support stop at final pose?");
-        newDyn.targetRelSpeed = 1.0;
         ptg->updateNavDynamicState(newDyn);
 
         // Inverse map of relative pose:
@@ -75,12 +73,13 @@ bool selfdrive::bestTrajectory(
         if (this_ptg_dist_at_target < best_ptg_target_dist)
         {
             best_ptg_target_dist = this_ptg_dist_at_target;
-            npa.ptg_index        = ptg_idx;
-            npa.ptg_path_index   = ptg_k;
-            npa.ptg_to_d         = ptg_dist;
-            npa.ptg_path_alpha   = ptg->index2alpha(ptg_k);
-            best_ptg_relPose     = reconstr_pose;
-            best_ptg_velAtEnd    = ptg->getPathTwist(ptg_k, ptg_step);
+
+            npa.ptg_index      = ptg_idx;
+            npa.ptg_path_index = ptg_k;
+            npa.ptg_to_d       = ptg_dist;
+            npa.ptg_path_alpha = ptg->index2alpha(ptg_k);
+            best_ptg_relPose   = reconstr_pose;
+            best_ptg_velAtEnd  = ptg->getPathTwist(ptg_k, ptg_step);
         }
 
     }  // end for each ptg
@@ -90,6 +89,8 @@ bool selfdrive::bestTrajectory(
     // take care of robot kinematics limitations:
     if (npa.ptg_index >= 0)
     {
+        if (VERBOSE) std::cout << "before: " << npa.state_to.asString() << "\n";
+
         // Correct pose:
         npa.state_to.pose = npa.state_from.pose + best_ptg_relPose;
 
@@ -97,6 +98,8 @@ bool selfdrive::bestTrajectory(
         npa.state_to.vel = best_ptg_velAtEnd;
         // local to global:
         npa.state_to.vel.rotate(npa.state_to.pose.phi);
+
+        if (VERBOSE) std::cout << " after: " << npa.state_to.asString() << "\n";
     }
 
     return true;
