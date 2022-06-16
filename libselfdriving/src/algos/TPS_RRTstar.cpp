5b08b535f61cf22cca84d2ac8eb55ce56cba128b
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2022-06-16 11:59:27 +0200

    copyright bump

--- libselfdriving/src/algos/TPS_RRTstar.cpp
+++ libselfdriving/src/algos/TPS_RRTstar.cpp
@@ -1,6 +1,6 @@
 ﻿/* -------------------------------------------------------------------------
  *   SelfDriving C++ library based on PTGs and mrpt-nav
- * Copyright (C) 2019-2021 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2019-2022 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 
