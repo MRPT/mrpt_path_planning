474c2d03f394f68c7bf14db194025bce56ca8141
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-09-07 10:55:07 +0200

    path interpolation

---
+++ libselfdriving/include/selfdriving/data/basic_types.h
@@ -0,0 +1,31 @@
+/* -------------------------------------------------------------------------
+ *   SelfDriving C++ library based on PTGs and mrpt-nav
+ * Copyright (C) 2019-2022 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+
+#pragma once
+
+#include <cstdlib>  // size_t
+
+namespace selfdriving
+{
+/** Distances measured by PoseDistanceMetric, or "pseudodistances" of PTGs, that
+ * is, distances along SE(2), including a weighted distance for rotations */
+using distance_t = double;
+
+/** TPS normalized distances in range [0,1] */
+using normalized_distance_t = double;
+
+/** Index of a trajectory in a PTG */
+using trajectory_index_t = int;
+
+using ptg_index_t = size_t;
+
+/** Normalized speed in range [0,1] */
+using normalized_speed_t = double;
+
+/** Time duration in seconds */
+using duration_seconds_t = double;
+
+}  // namespace selfdriving
