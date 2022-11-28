a4a5a5b5a3eabae20be5f5cf21dfcaea0cbebaaa
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-11-28 22:46:45 +0100

    move internal function to its own header

--- libselfdriving/src/algos/TPS_Astar.cpp
+++ libselfdriving/src/algos/TPS_Astar.cpp
@@ -7,6 +7,7 @@
 #include <mrpt/math/wrap2pi.h>
 #include <mrpt/opengl/COpenGLScene.h>
 #include <selfdriving/algos/TPS_Astar.h>
+#include <selfdriving/algos/edge_interpolated_path.h>
 #include <selfdriving/algos/render_tree.h>
 #include <selfdriving/algos/tp_obstacles_single_path.h>
 #include <selfdriving/algos/transform_pc_square_clipping.h>
@@ -79,72 +80,6 @@ TPS_Astar_Parameters TPS_Astar_Parameters::FromYAML(
 TPS_Astar::TPS_Astar() : mrpt::system::COutputLogger("TPS_Astar")
 {
     profiler_().setName("TPS_Astar");
-}
-
-void edge_interpolated_path(
-    MoveEdgeSE2_TPS& edge, const TrajectoriesAndRobotShape& trs,
-    const std::optional<mrpt::math::TPose2D>& reconstrRelPoseOpt = std::nullopt,
-    const std::optional<size_t>&              ptg_stepOpt        = std::nullopt,
-    const std::optional<size_t>&              numSegments        = std::nullopt)
-{
-    size_t nSeg = 0;
-
-    if (numSegments.has_value()) { nSeg = *numSegments; }
-    else
-    {
-        // Use same number than existing edge interpolated path:
-        ASSERT_(edge.interpolatedPath.size() > 1);
-        nSeg = edge.interpolatedPath.size();
-    }
-
-    auto& ptg = trs.ptgs.at(edge.ptgIndex);
-
-    const duration_seconds_t dt = ptg->getPathStepDuration();
-
-    mrpt::math::TPose2D reconstrRelPose;
-    if (reconstrRelPoseOpt.has_value())
-        reconstrRelPose = reconstrRelPoseOpt.value();
-    else
-    {
-        MRPT_TODO("continue...");
-        THROW_EXCEPTION("To do");
-
-        // ptg->inverseMap_WS2TP(edgePoseIncr.x, edgePoseIncr.y)
-    }
-
-    size_t ptg_step = 0;
-    if (ptg_stepOpt)
-    {  //
-        ptg_step = *ptg_stepOpt;
-    }
-    else
-    {
-        MRPT_TODO("continue...");
-        THROW_EXCEPTION("To do");
-    }
-
-    auto& ip = edge.interpolatedPath;
-    ip.clear();
-
-    // t=0: fixed start relative pose
-    // ---------------------------------
-    ip[0 * dt] = {0, 0, 0};
-
-    // interpolated path in between start and goal
-    // ----------------------------------------------
-    for (size_t i = 0; i < nSeg; i++)
-    {
-        const auto               iStep = ((i + 1) * ptg_step) / (nSeg + 2);
-        const duration_seconds_t t     = iStep * dt;
-        ip[t] = ptg->getPathPose(edge.ptgPathIndex, iStep);
-    }
-
-    // Final, known pose and time
-    // ----------------------------------------------
-    ip[ptg_step * dt] = reconstrRelPose;
-
-    // Motion execution time:
-    edge.estimatedExecTime = ptg_step * dt;
 }
 
 PlannerOutput TPS_Astar::plan(const PlannerInput& in)
